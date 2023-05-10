febd36e10f6e9b3e090ff2a86ff3b9bef8386f96
JGrant024 <jonathan.grant23@icloud.com>
2023-05-09 23:26:08 -0400

    updated styling removed buttons & still working on functions

--- main.js
+++ main.js
@@ -3,29 +3,27 @@ let petName = "Mali";
 let hungerLevel = 100;
 let mood = "Angry";
 let moodLevel = "0";
-let sleepLevel = "Sleepy";
 
 // Get DOM elements for the pet's status
 const petNameEl = document.querySelector("#pet-name");
 const hungerEl = document.querySelector("#hunger-level");
 const moodEl = document.querySelector("#mood");
 const moodLevelEl = document.querySelector("#mood-level");
-const sleepLevelEl = document.querySelector("#sleep-level");
-const codeLevelEl = document.querySelector("code-level");
 
 // Get DOM elements for the action buttons
 const feedButton = document.getElementById("feed-button");
 const playButton = document.getElementById("play-button");
-const sleepingButton = document.getElementById("sleep-button");
-const codeButton = document.getElementById("code-button");
+
+// TODO: The way we update and use hungerLevel can cause unexpected results
+// Ask the students if they can think of a way to improve the code
+
+// TODO: Ask the students to explain type coercion
+// I found the following documentation on type coercion https://www.scaler.com/topics/javascript/type-coercion-javascript/
 
 // Function to update the pet's status when fed
 function feedPet() {
-  // TODO: The way we update and use hungerLevel can cause unexpected results
-  // Ask the students if they can think of a way to improve the code
-  if (hungerLevel >= 10) {
+  if (hungerLevel >= 100) {
     hungerLevel -= 10;
-    // TODO: Ask the students to explain type coercion
     hungerEl.innerText = hungerLevel + "%";
     mood = "Happy";
     moodEl.innerText = mood;
@@ -36,40 +34,22 @@ function feedPet() {
     moodEl.innerText = mood;
   }
 }
-
-// function to update the pet's sleep level
-
-function makePetSleep() {
-  if (sleepLevel < 40) {
-    sleepLevel += 100;  
-    sleepLevelEl.innerText = Sleepy;
-  } else {
-    sleepLevel = 0
-  }
-}
-
 // Function to update the pet's status when played with
 function playWithPet() {
-  if (hungerLevel < 90) {
+  if (hungerLevel < 100) {
     hungerLevel += 10;
     hungerEl.innerText = hungerLevel + "%";
     mood = "Excited";
     moodEl.innerText = mood;
-    r;
   } else {
     mood = "Bored";
     moodEl.innerText = mood;
   }
 }
-playWithPet();
-
-// function codeButton() {
-//  if (codeButton)
-// }
 
 function showMessage() {
   alert(
-    "Hi there! Welcome to Mali's world! We love to Eat, Sleep and Develop Code! Come and hang out with me!"
+    "Hi there! Welcome to MaliCat's world! We love to Eat, Sleep and Develop Code! Come and hang out with me!"
   );
 }
 
@@ -78,4 +58,3 @@ showMessage();
 // Add event listeners to the action buttons
 feedButton.addEventListener("click", feedPet);
 playButton.addEventListener("click", playWithPet);
-sleepingButton.addEventListener("click", makePetSleep);
